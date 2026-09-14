c1ec6d0b6ad2d17ff353a3b4effdfb6cafda71ef
Kushal Jain <jainkushal556@gmail.com>
2026-09-15 00:36:06 +0530

    cors update

--- Web_Chatbot/backend_main.py
+++ Web_Chatbot/backend_main.py
@@ -18,11 +18,17 @@ class stateless_ai_message(BaseModel):
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:5501",
+    "http://localhost:5501",
+    "https://gemini-context-aware-chatbot.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"]
 )
 
